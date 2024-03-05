3b0413448812383002e0dce2da9206940268d95d
Chaitanya Vuddanti <52860953+chaitanya608@users.noreply.github.com>
2024-03-05 12:41:17 +0530

    minor correction

--- src/routes/healthRouter.ts
+++ src/routes/healthRouter.ts
@@ -2,7 +2,6 @@ import express from "express";
 
 const router = express.Router();
 
-// Self signup
 router.get("/", async (req, res) => {
   return res.status(200).json("application is live");
 });
